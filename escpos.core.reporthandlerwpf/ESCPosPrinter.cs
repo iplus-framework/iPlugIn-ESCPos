3d56207c2fcd2dace902929314d6f1ff0bb5f4f9
Damir Lisak <dlisak@kajbum.com>
2026-03-03 11:55:41 +0100

    async

--- escpos.core.reporthandlerwpf/ESCPosPrinter.cs
+++ escpos.core.reporthandlerwpf/ESCPosPrinter.cs
@@ -30,9 +30,9 @@ namespace escpos.core.reporthandlerwpf
             return true;
         }
 
-        public override Task<bool> ACDeInit(bool deleteACClassTask = false)
+        public override async Task<bool> ACDeInit(bool deleteACClassTask = false)
         {
-            return base.ACDeInit(deleteACClassTask);
+            return await base.ACDeInit(deleteACClassTask);
         }
         #endregion
 
